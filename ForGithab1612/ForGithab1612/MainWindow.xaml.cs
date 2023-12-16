250d27a46ba239c8b29b010ffa89b06af263ae6d
KIRILL1STARNIKOV <151497538+KIRILL1STARNIKOV@users.noreply.github.com>
2023-12-16 12:14:54 +0900

    Add files via upload

---
+++ ForGithab1612/ForGithab1612/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ForGithab1612
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        int check =0;
+        private void Button_Click (object sender, RoutedEventArgs e)
+        {
+            check++;
+            string ss = $"NEW element{check} ";
+           listb.Items.Insert(0, ss);
+                
+        }
+        private void Butsum_Click(object sender, RoutedEventArgs e)
+        {
+            int firstnumeber = Convert.ToInt32(tbfirstnumber.Text);
+            int secondnumber = Convert.ToInt32(tbsecondnumber.Text);
+            int result = firstnumeber + secondnumber;
+            MessageBox.Show("result sum "+ result,"Messange");
+        }
+    }
+}
